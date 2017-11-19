1b6a9f2f5d22130c3bab68e22035579dff9d4086
Jordan Weaver <jww@q.com>
2017-11-18 23:57:02 -0800

    completed logic for alarm and timer. Havent implemented TCNT1 or TCNT3.

--- lab4_submit/lab4.c
+++ lab4_submit/lab4.c
@@ -18,10 +18,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include "hd44780.h"
+#include <string.h>
 
 //-------------------------------
 //LCD STUFF
-char    lcd_string_array[32] = {36,36,36,36,36,36,36,36};  //holds a string to refresh the LCD
+char    lcd_string_array[32] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+								' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',};  //holds a string to refresh the LCD
 //------------------------------
 
 //holds data to be sent to the segments. logic zero turns segment on
@@ -42,6 +44,11 @@ uint8_t sec = 0;
 uint8_t update_LED = 0;
 uint8_t minutes = 0;
 uint8_t hours = 0;
+uint8_t alarm_set = 0;
+uint8_t alarm_hours = 0;
+uint8_t alarm_minutes = 0;
+char lcd_str_minutes[16];  //holds string to send to lcd  
+char lcd_str_hour[16];  //holds string to send to lcd 
 
 //*****************************************************************************
 //							bin_to_bcd
@@ -111,14 +118,17 @@ void segsum(uint16_t bcd) {
 //**********************************************************************
 void spi_init(void){
   DDRB  |= 0x07; //output mode for SS, MOSI, SCLK
+  PORTB |= _BV(PB1);  //port B initalization for SPI, SS_n off
   //set ss low
   //PORTB &= ~(1<<PB0);
 
   SPCR   = (1<<MSTR) | (1<<SPE); //master mode, clk low on idle, leading edge sample
 
   SPSR   = (1<<SPI2X); //choose double speed operation
-  //SPCR |= (1<<SPR1) | (1<<SPR0);  //clock slowest
-  //SPCR |= (1<<CPOL) | (1<<CPHA);  // clock idle high
+  
+  /* Run this code before attempting to write to the LCD.*/
+  DDRF  |= 0x08;  //port F bit 3 is enable for LCD
+  PORTF &= 0xF7;  //port F bit 3 is initially low
  }//spi_init
 //**********************************************************************
 
@@ -127,13 +137,21 @@ void spi_init(void){
 //
 //
 void tcnt0_init(void){
-/*
-  TIMSK |= (1<<TOIE0);             //enable interrupts
-  TCCR0 |= (1<<CS02) | (1<<CS00);  //normal mode, prescale by 128
- */
- TIMSK |= (1<<TOIE0);
- ASSR |= (1<<AS0);
- TCCR0 |= (1<<CS00);
+ TIMSK |= (1<<TOIE0);	//enable overflow interrupt
+ ASSR |= (1<<AS0);		//use external 32kHz clock
+ TCCR0 |= (1<<CS00);	//no prescale
+}
+//*************************************************************************
+
+//***********************************************************************
+//                              tcnt0_init                             
+//
+//
+void tcnt2_init(void){
+ //fast PWM mode, non-inverting, clck/8 prescale
+ TCCR2 |= (1<<CS21) | (1<<COM21) | (1<<COM20) | (1<<WGM20) | (1<<WGM21);
+ //start on brightests
+ OCR2 = 1;
 }
 //*************************************************************************
 
@@ -174,6 +192,14 @@ ISR(TIMER0_OVF_vect){
 }
 //*******************************************************************************
 
+//clears the lcd_string_array
+void clear_lcd_array(void) {
+	int i = 0;
+	for(i = 0; i < 32; i++) {
+		lcd_string_array[i] = ' ';
+	}
+}
+
 uint8_t main()
 {
 //set port B bits 4-7 as outputs
@@ -185,18 +211,38 @@ DDRC = (1<<DDC0) | (1<<DDC1);
 PORTC = 0;
 
 tcnt0_init();  //initalize counter timer zero
+tcnt2_init();  //initalize counter timer two
 spi_init();    //initalize SPI port
 lcd_init();    //initalize LCD (lcd_functions.h)
+clear_display();
 sei();         //enable interrupts before entering loop
 
 int digit_count = 0;
 int i = 0;
+
+//Initalize ADC and its ports DDRF  &= ~(_BV(DDF7)); //make port F bit 7 is ADC input  
+PORTF &= ~(_BV(PF7));  //port F bit 7 pullups must be off
+                       
+ADMUX = (1<<REFS0) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0); //single-ended, input PORTF bit 7, right adjusted, 10 bits
+ADCSRA = (1<<ADEN) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2); //ADC enabled, don't start et, single shot mode
+															//division factor is 128 (125khz)
 while(1){
+	
+//ADC--------------------------------------------------------------------------------
+	ADCSRA |= (1<<ADSC);    // poke ADSC and start conversion
+	while (bit_is_clear(ADCSRA,ADIF)){}; // spin while interrupt flag not set
+
+	ADCSRA |= (1<<ADIF); //its done, clear flag by writing a one
+
+	OCR2 = (ADC & 0xff);                      //read the ADC output as 16 bits
+//END ADC---------------------------------------------------------------------------
+
+//Buttons---------------------------------------------------------------------------
 	//make PORTA an input port with pullups 
 	DDRA = 0;
 	PORTA = 0xff;
 	//enable tristate buffer for pushbutton switches
-	PORTB = (1<<PB4) | (1<<PB5) | (1<<PB6);
+	PORTB |= (1<<PB4) | (1<<PB5) | (1<<PB6);
 	//now check each button and increment the count as needed
 	//uint8_t button_press = 0;
 	for(i = 0; i < 8; i++) {
@@ -205,10 +251,31 @@ while(1){
 		  //turn mode off at button if its on
 		switch(i) {
 			case 0:
-				minutes++;
+				minutes = 0;
+				hours = 0;
+				alarm_minutes = 0;
+				alarm_hours = 0;
+				alarm_set = 0;
 				break;
 			case 1:
+				minutes++;
+				break;
+			case 2:
 				hours++;
+				break;
+			case 3:
+				//toggle alarm_set flag to turn alarm on/off
+				alarm_set ^= 0x01;
+				break;
+			case 4:
+				alarm_minutes++;
+				break;
+			case 5:
+				alarm_hours++;
+				break;
+			case 6:
+				break;
+			case 7:
 				break;
 		}
 	  }
@@ -217,13 +284,15 @@ while(1){
 	//disable tristate buffer for pushbutton switches
 	PORTB |= (1<<PB5) | (1<<PB6);  //enables unused Y6 output
 	PORTB &= ~(1<<PB4);
+//END BUTTONS-----------------------------------------------------------------------------
 
+//LED DISPLAY-----------------------------------------------------------------------------
   //adjust time for overflow
   if(sec > 59) {
 	  minutes++;
 	  sec = 0;
   }
-  if(minutes > 60) {
+  if(minutes > 59) {
 	  hours++;
 	  minutes = 0;
   }
@@ -244,6 +313,7 @@ while(1){
   PORTB &= portb_digit_and[digit_count];
   //update digit to display
   digit_count++;
+  //remove leading zero from clock
   if((digit_count == 4) & (hours < 10)) {
 	  digit_count++;
   }
@@ -251,8 +321,42 @@ while(1){
   if(digit_count > 4) {
 	  digit_count = 0;
   }
-  refresh_lcd("REFLEX TESTER");
-  //insert loop delay for display
+//END LED DISPLAY-----------------------------------------------------------------------------
+
+//LCD DISPLAY---------------------------------------------------------------------------------
+  //check for minutes or hour overflow
+  if(alarm_minutes > 59) {
+	  alarm_hours++;
+	  alarm_minutes = 0;
+  }
+  if(alarm_hours > 24) {
+	  alarm_hours = 0;
+  }
+  //clean out the lcd_string_array
+  clear_lcd_array();
+  //convert minutes from int to string
+  itoa(alarm_minutes, lcd_str_minutes, 10);
+  //convert hours from int to string
+  itoa(alarm_hours, lcd_str_hour, 10);
+  //copy string minutes and hours strings into master string
+  strcpy(lcd_string_array, lcd_str_hour);
+  strcat(lcd_string_array, ":");
+  //add a leading 0 if minutes < 10
+  if(alarm_minutes < 10) {
+	  strcat(lcd_string_array, "0");
+  }
+  strcat(lcd_string_array, lcd_str_minutes);
+  //indicate whether the alarm is set
+  if(alarm_set) {
+	  strcat(lcd_string_array, " ALARM");
+  }
+  
+  //delete null terminator from string functions
+  lcd_string_array[strlen(lcd_string_array)] = ' ';
+  //write the master string to the lcd
+  refresh_lcd(lcd_string_array);
+//END LCD DISPLAY---------------------------------------------------------------------------------  
+
   _delay_ms(1);
   }//while
 }//main
